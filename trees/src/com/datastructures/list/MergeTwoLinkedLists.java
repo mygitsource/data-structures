19b4ccf57efb260e966521dccf13bb1aefc4721d
Ravi Pasala <raviaz@aol.com>
2014-01-18 18:51:48 +0530

    modified mergetwolinkedlist

--- trees/src/com/datastructures/list/MergeTwoLinkedLists.java
+++ trees/src/com/datastructures/list/MergeTwoLinkedLists.java
@@ -1,5 +1,7 @@
 package com.datastructures.list;
 
+import com.datastructures.trees.util.Util;
+
 /*
  * 
 ===========Merge a linked list into another linked list at alternate positions============
@@ -20,7 +22,48 @@ insert nodes of second list into first list at alternate positions of first list
  */
 public class MergeTwoLinkedLists {
 
-	void mergeTwoLinkedLists(ListNode listNode1, ListNode listNode2){
+	void mergeTwoLinkedLists(LinkedList list1, LinkedList list2){
+		ListNode head1 = list1.getHead();
+		ListNode head2 = list2.getHead();
 		
+		ListNode current = head1;
+		
+		while(current != null){
+			//before going to start linking , get the next current and next newhead nodes
+			ListNode nextCurr = current.getNext();   // get next current in list1
+			ListNode newHead = head2.getNext();	// get the list2 head
+			
+			//set current node next to head of the list2 and 
+			current.setNext(head2);
+			//set head2
+			head2.setNext(nextCurr);
+			//move currentNode to nextCurrent node and head(list2) to new head.
+			current = nextCurr;
+			head2 = newHead;	
+			
+			list2.setHead(head2);
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		LinkedList list1 = new LinkedList();
+		list1.insertNode(1);
+		list1.insertNode(3);
+		list1.insertNode(5);
+		list1.insertNode(7);
+		LinkedList list2 = new LinkedList();
+		list2.insertNode(2);
+		list2.insertNode(4);
+		list2.insertNode(6);
+		list2.insertNode(8);
+		list2.insertNode(10);
+		list2.insertNode(12);
+		Util.printList(list1);
+		Util.printList(list2);
+		MergeTwoLinkedLists lists = new MergeTwoLinkedLists();
+		lists.mergeTwoLinkedLists(list1, list2);
+		Util.printList(list1);
+		Util.printList(list2);
 	}
 }
